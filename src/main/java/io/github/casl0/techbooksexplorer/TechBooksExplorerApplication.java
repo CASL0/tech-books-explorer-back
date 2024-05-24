9430d0609e0a376f2b007a9042dbea04f19160ad
CASL0 <gin.0046.822@gmail.com>
2024-05-24 20:37:50 +0900

    chore: spring初期プロジェクト

---
+++ src/main/java/io/github/casl0/techbooksexplorer/TechBooksExplorerApplication.java
@@ -0,0 +1,13 @@
+package io.github.casl0.techbooksexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechBooksExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechBooksExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/casl0/techbooksexplorer/TechBooksExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.casl0.techbooksexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBooksExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
