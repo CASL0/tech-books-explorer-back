cb05f399f4fe792f2652dc2d8fa9c028321d3a1e
CASL0 <gin.0046.822@gmail.com>
2024-05-25 18:42:28 +0900

    chore: 出版社マスターデータ

---
+++ src/main/resources/db/migration/V2__insert_publishers_master_data.sql
@@ -0,0 +1,15 @@
+INSERT IGNORE INTO publishers (id, name, url)
+VALUES
+  (
+    1,
+    'オライリー・ジャパン',
+    'https://www.oreilly.co.jp/catalog/'
+  );
+
+INSERT IGNORE INTO publishers (id, name, url)
+VALUES
+  (2, '翔泳社', 'https://www.shoeisha.co.jp/book/list');
+
+INSERT IGNORE INTO publishers (id, name, url)
+VALUES
+  (3, '技術評論社', 'https://gihyo.jp/book/genre');
