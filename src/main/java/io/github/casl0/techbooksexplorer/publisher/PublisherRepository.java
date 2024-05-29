c89807ab6dc7c7bedf85e531a38ed1f3ec4706b0
CASL0 <gin.0046.822@gmail.com>
2024-05-30 00:48:22 +0900

    feat: 出版社データへのアクセス用リポジトリ

---
+++ src/main/java/io/github/casl0/techbooksexplorer/publisher/PublisherRepository.java
@@ -0,0 +1,20 @@
+package io.github.casl0.techbooksexplorer.publisher;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Publisherオブジェクトへのエントリポイントとなるリポジトリ
+ */
+@Repository
+public interface PublisherRepository extends JpaRepository<Publisher, Integer> {
+    /**
+     * 出版社名からPublisherオブジェクトを取得します
+     * 
+     * @param name 出版社名
+     * @return 見つかったPublisherオブジェクト、見つからなかった場合はnull
+     */
+    Optional<Publisher> findFirstByName(String name);
+}
