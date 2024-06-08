bcaf88f47eb4513af4aafd58e1f745bb1620487c
CASL0 <gin.0046.822@gmail.com>
2024-06-09 00:41:01 +0900

    fix: 価格のフィールドにバリデーションを追加

--- src/main/java/io/github/casl0/techbooksexplorer/book/BookRequest.java
+++ src/main/java/io/github/casl0/techbooksexplorer/book/BookRequest.java
@@ -4,6 +4,7 @@ import org.hibernate.validator.constraints.ISBN;
 import org.hibernate.validator.constraints.URL;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -30,6 +31,8 @@ public class BookRequest {
     /**
      * 価格
      */
+    @Pattern(regexp = "^￥\\d{1,3}(,\\d{3})*$")
+    @Size(max = 10)
     private String price;
 
     /**
